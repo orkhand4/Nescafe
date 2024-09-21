53a1f638dae971ba953e72ea4a7513d1857aa9ad
Orkhand4 <orkhand4@mail.ru>
2024-09-21 11:21:57 +0400

    Range

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const inpt_amount = document.querySelector('#amount');
 const inpt_totalAmount = document.querySelector('#totalAmount');
 const inpt_monthlyAmount = document.querySelector('#monthlyAmount');
+const span_yearCount = document.querySelector('#yearCount');
+const inpt_year = document.querySelector('#year');
+
+
+inpt_year.addEventListener('input', () => {
+  span_yearCount.innerText = inpt_year.value;
+});
+
 
 inpt_amount.addEventListener('input', () => {
-  const amount = +inpt_amount.value; // 10000
+  const amount = +inpt_amount.value
 
   const percent = 14
 
