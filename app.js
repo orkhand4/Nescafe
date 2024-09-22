f8e4f04f24cd5175a0f45d9529fab697db2628e6
Orkhand4 <orkhand4@mail.ru>
2024-09-22 10:21:40 +0400

    Filter

--- app.js
+++ app.js
@@ -5,11 +5,27 @@ const span_yearCount = document.querySelector('#yearCount');
 const inpt_year = document.querySelector('#year');
 const span_percentCount = document.querySelector('#percentCount');
 
+const calculate = (amount, year, percent) => {
+  const result = amount + amount *(percent / 100);
+  inpt_totalAmount.value = result.toFixed(2);
+  
+  inpt_monthlyAmount.value
+}
+
+const getInfo = () => {
+
+}
+
 inpt_year.addEventListener('input', () => {
   const year = inpt_year.value
   span_yearCount.innerText = year;
 
-  span_percentCount.innerText = (12+year/3).toFixed(1)
+  const percent = (12+year/3).toFixed(1)
+
+  span_percentCount.innerText = percent
+
+  const amount = +inpt_amount.value;
+  calculate(amount, year, percent)
 });
 
 
